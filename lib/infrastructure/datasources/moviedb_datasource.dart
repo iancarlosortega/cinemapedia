5314fbe96db819fb7af890a4c05e2f22e78f2252
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2023-05-28 11:57:01 -0500

    Fin Sección 15

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -69,4 +69,14 @@ class MoviedbDatasource extends MoviesDataSource {
     final Movie movie = MovieMapper.movieDetailsToEntity(movieDetails);
     return movie;
   }
+
+  @override
+  Future<List<Movie>> searchMovies(String query) async {
+    if (query.isEmpty) return [];
+
+    final response =
+        await dio.get('/search/movie', queryParameters: {'query': query});
+
+    return _jsonToMovies(response.data);
+  }
 }
